8adff92b6b42d6a8026158d4ad353ce96f490756
labgambino <113803192+labgambino@users.noreply.github.com>
2025-02-08 22:53:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+setTimeout(function(){ alert("powered by BERMUDEZ (2025)"); }, 3000);
+
+function ir (){
+    
+var c=2244; 
+var u="inaser"; 
+    if(document.form.password.value==c && document.form.login.value==u){
+    alert ("INASER: Acceso permitido al EXPEDIENTE 202X 9X2X0X-prototipo. \nTodos los Derechos reservados. (2025)"); 
+window.location="hiperenlopag.html";} 
+    
+    else { 
+        alert("Por favor, ingresa tu usuario y contraseña correctos");
+    }
+     }
+
